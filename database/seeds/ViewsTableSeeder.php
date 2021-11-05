6c54fa1a159aa6a937a8534310ea60e1da67e149
bePIRRO <manuel.pirro@gmail.com>
2021-11-05 15:23:56 +0100

    add views seeder

---
+++ database/seeds/ViewsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\View;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ViewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $views = new view();
+
+            $views->day = $faker->dateTime();
+            $views->count = $faker->randomDigit();
+            $views->ip_address = $faker->ipv4();
+            $views->save();
+        }
+    }
+}
