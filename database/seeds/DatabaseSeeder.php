f9744ad7ee715988684bf64ab82a3128f4b6082e
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-11-07 22:05:21 +0100

    Add Services Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\models\Service;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,10 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersTableSeeder::class,
             MessagesTableSeeder::class,
             ViewsTableSeeder::class,
             SponsorshipsTableSeeder::class,
+            ServicesTableSeeder::class,
         ]);
     }
 }
