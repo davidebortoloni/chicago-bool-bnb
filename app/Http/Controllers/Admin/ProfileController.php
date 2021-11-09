8a0048bc7a43ea80abfe70e0cb06974f0a63c2b9
davidebortoloni <davidebortoloni@gmail.com>
2021-11-09 23:20:28 +0100

    Inserimento profile (view e controller)

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+        return view('profile', compact('user'));
+    }
 }
