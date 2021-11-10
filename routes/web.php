e4ac680cdb40a12a5feecea5c23f35e6fc176ca4
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-11-10 16:57:47 +0100

    add web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/', 'Homecontroller@index')->name('home');
+});
+
+Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
+    Route::resource('sponsorships', 'SponsorshipController');
+});
