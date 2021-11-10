18eea8189ec00e8036072193a7f140723891749d
bePIRRO <manuel.pirro@gmail.com>
2021-11-10 17:42:58 +0100

    change home route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/admin') }}">Home</a>
+                        <a href="{{ route('admin.apartments.index') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
