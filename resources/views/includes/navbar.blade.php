9d496f25271413bade97e00d4aedcab00d54587b
Simichus <simonefilippobilardo@gmail.com>
2021-11-10 18:05:26 +0100

    modificati conflitti

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -33,7 +33,7 @@
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
-                            <a class="dropdown-item" href="{{ route('admin.apartments.index') }}">
+                            <a class="dropdown-item" href="{{ route('apartments.index') }}">
                                 Dashboard
                             </a>
 
