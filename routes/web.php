57659443ae6cdf36c24d6bda4af6ca73d3bdec99
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-11-10 16:54:26 +0100

    Add Web Routes Sponsorship

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', 'Homecontroller@index')->name('home');
 });
 
-Auth::routes(['register' => true]);
-
-//! Routes admin
-Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function() {
-    Route::get('/', 'HomeController@index')->name('.home');
+Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
+    Route::resource('sponsorships', 'SponsorshipController');
 });
