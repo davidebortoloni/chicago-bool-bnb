6feb25f29a40aa98e5e91a2f44db2c7efb9feb80
bePIRRO <manuel.pirro@gmail.com>
2021-11-12 12:04:11 +0100

    header style

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -3,12 +3,22 @@
         <h1 class="mt-5">BoolBnB</h1>
         <div class="container">
             <div class="row mt-5">
-                <div class="col-11">
+                <div class="col-9">
                     <input
                         type="text"
-                        class="form-control"
+                        class="form-control search-bar"
                         placeholder="Dove vuoi andare?"
                     />
+                </div>
+                <div
+                    class="
+                        col-2
+                        d-flex
+                        align-items-center
+                        justify-content-center
+                    "
+                >
+                    <a href="" class="adv-src"> Ricerca avanzata </a>
                 </div>
                 <div class="col-1">
                     <button type="button" class="btn btn-success">Cerca</button>
